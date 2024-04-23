1a94d0f9a7c8c3c3260cbf68d684f198f0db4883
Roddeman, JA <julian.roddeman@rabobank.nl>
2024-04-23 11:52:03 +0200

    baseline things

--- baseline_model/export2onnx.py
+++ baseline_model/export2onnx.py
@@ -1,8 +1,9 @@
 import os
-import torch
-from torch import onnx as onnx
+
 import numpy as np
+import torch
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+from torch import onnx as onnx
 
 # Build the algorithm docker container first using '. build.sh' then run this command
 # docker run -it --entrypoint bash -v $(pwd):/output uls23:latest
@@ -17,7 +18,7 @@ if __name__ == "__main__":
         device=torch.device("cpu"),
         verbose=False,
         verbose_preprocessing=False,
-        allow_tqdm=False
+        allow_tqdm=False,
     )
     # Initialize the network architecture, loads the checkpoint of the model you want to export
     predictor.initialize_from_trained_model_folder(
@@ -39,5 +40,11 @@ if __name__ == "__main__":
     # Export the model
     print("Saving model...")
     os.makedirs("/output/onnx_model/")
-    torch.onnx.export(model, torch.from_numpy(dummy_input).type(torch.float), "/output/onnx_model/ULS_nnUnet.onnx", export_params=True, verbose=True)
+    torch.onnx.export(
+        model,
+        torch.from_numpy(dummy_input).type(torch.float),
+        "/output/onnx_model/ULS_nnUnet.onnx",
+        export_params=True,
+        verbose=True,
+    )
     print("Finished :)")
